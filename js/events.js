16f1cd189706f041fb38bed61200726ee3fb8bef
lucash <lucashcardoso26@gmail.com>
2023-01-16 10:15:49 -0300

    modified events.js

--- js/events.js
+++ js/events.js
@@ -1,57 +1,129 @@
 import {
-    buttonPlay,
-    buttonPause,
-    buttonStop,
-    buttonIncrease,
-    buttonDecrease,
-    soundForest,
-    soundRain,
-    soundCoffee,
-    soundFireplace,
-} from './elements.js';
+    btnPlay,
+    btnPause,
+    btnStop,
+    btnVolumeUp,
+    btnVoumeDown,
+    btnForest,
+    btnRain,
+    btnCoffee,
+    btnFireplace,
+    forestCard,
+    rainCard,
+    coffeeCard,
+    fireplaceCard,
+    sliderVolumeForest,
+    sliderVolumeRain,
+    sliderVolumeCoffee,
+    sliderVolumeFireplace,
+} from './elements.js'
 
 export default function Events({controls, timer, sound}) {
-    buttonPlay.addEventListener('click', () => {
+    btnPlay.addEventListener('click', function() {
         controls.play();
         timer.countDown();
-        sound.pressButton();
+        sound.pressButton()
     })
 
-    buttonPause.addEventListener('click', () => {
+    btnPause.addEventListener('click', function() {
         controls.pause();
-        timer.freezeDisplay();
-        sound.pressButton();
+        timer.freezeDisplay()
+        sound.pressButton()
+
     })
 
-    buttonStop.addEventListener('click', () => {
-        timer.reset();
-        controls.reset();
-        sound.pressButton();
+    btnStop.addEventListener('click', function() {
+        controls.reset()
+        timer.reset()
+        sound.pressButton()
     })
 
-    buttonIncrease.addEventListener('click', () => {
+    btnVolumeUp.addEventListener('click', function() {
         timer.increaseDisplay();
         sound.pressButton();
     })
 
-    buttonDecrease.addEventListener('click', () => {
+    btnVoumeDown.addEventListener('click', function() {
         timer.decreaseDisplay();
         sound.pressButton();
     })
 
-    soundForest.addEventListener('click', () => {
-        sound.forestPress();
+    btnForest.addEventListener('click', function() {
+        if (forestCard.classList.contains('enable')) {
+            forestCard.classList.remove('enable')
+            sound.forestMusic.pause()
+            return
+        }
+
+        forestCard.classList.add('enable')
+        rainCard.classList.remove('enable')
+        coffeeCard.classList.remove('enable')
+        fireplaceCard.classList.remove('enable')
+        sound.forestMusic.play()
+        sound.rainMusic.pause()
+        sound.coffeeMusic.pause()
+        sound.fireMusic.pause()
     })
 
-    soundRain.addEventListener('click', () => {
-        sound.rainPress();
+    btnRain.addEventListener('click', function() {
+        if (rainCard.classList.contains('enable')) {
+            rainCard.classList.remove('enable')
+            sound.rainMusic.pause()
+            return
+        }
+
+        rainCard.classList.add('enable')
+        forestCard.classList.remove('enable')
+        coffeeCard.classList.remove('enable')
+        fireplaceCard.classList.remove('enable')
+        sound.forestMusic.pause()
+        sound.rainMusic.play()
+        sound.coffeeMusic.pause()
+        sound.fireMusic.pause()
     })
 
-    soundCoffee.addEventListener('click', () => {
-        sound.coffeePress();
+    btnCoffee.addEventListener('click', function() {
+        if (coffeeCard.classList.contains('enable')) {
+            coffeeCard.classList.remove('enable')
+            sound.coffeeMusic.pause()
+            return
+        }
+        coffeeCard.classList.add('enable')
+        rainCard.classList.remove('enable')
+        forestCard.classList.remove('enable')
+        fireplaceCard.classList.remove('enable')
+        sound.forestMusic.pause()
+        sound.rainMusic.pause()
+        sound.coffeeMusic.play()
+        sound.fireMusic.pause()
     })
 
-    soundFireplace.addEventListener('click', () => {
-        sound.firePress();
+    btnFireplace.addEventListener('click', function() {
+        if (fireplaceCard.classList.contains('enable')) {
+            fireplaceCard.classList.remove('enable')
+            sound.fireMusic.pause()
+            return
+        }
+        
+        fireplaceCard.classList.add('enable')
+        coffeeCard.classList.remove('enable')
+        rainCard.classList.remove('enable')
+        forestCard.classList.remove('enable')
+        sound.forestMusic.pause()
+        sound.rainMusic.pause()
+        sound.coffeeMusic.pause()
+        sound.fireMusic.play()
     })
+
+    sliderVolumeForest.addEventListener('mousemove', setvolume)
+    sliderVolumeRain.addEventListener('mousemove', setvolume)
+    sliderVolumeCoffee.addEventListener('mousemove', setvolume)
+    sliderVolumeFireplace.addEventListener('mousemove', setvolume)
+
+    function setvolume() {
+        sound.forestMusic.volume = sliderVolumeForest.value
+        sound.rainMusic.volume = sliderVolumeRain.value
+        sound.coffeeMusic.volume = sliderVolumeCoffee.value
+        sound.fireMusic.volume = sliderVolumeFireplace.value
+    } 
 }
